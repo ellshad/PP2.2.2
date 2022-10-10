f394e85fc7303816da764490c437f8b58f4ae38b
Elshad <p0zd@ya.ru>
2022-10-10 11:59:26 +0300

    fix

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -4,12 +4,17 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import web.service.CarServiceImpl;
+import web.service.CarService;
 
 @Controller
 public class CarController {
 
-    CarServiceImpl service = new CarServiceImpl();
+    private final CarService service;
+
+    public CarController(CarService service) {
+        this.service = service;
+    }
+
     @GetMapping(value = "cars")
     public String show(@RequestParam(defaultValue = "5") int count, Model model){
         model.addAttribute("cars", service.getCarByCount(count));
--- src/main/java/web/service/CarServiceImpl.java
+++ src/main/java/web/service/CarServiceImpl.java
@@ -1,15 +1,21 @@
 package web.service;
 
+import org.springframework.stereotype.Service;
 import web.dao.CarDao;
-import web.dao.CarDaoImpl;
 import web.model.Car;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Service
 public class CarServiceImpl implements CarService {
 
-    CarDao carDao = new CarDaoImpl();
+    private final CarDao carDao;
+
+    public CarServiceImpl(CarDao carDao) {
+        this.carDao = carDao;
+    }
+
 
     public List<Car> getCarByCount(int count) {
         return carDao.getCars().stream().limit(count).collect(Collectors.toList());
